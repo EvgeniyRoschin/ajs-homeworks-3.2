809ab094a983318367fc6ef7ce6b15e19b8a0754
Evgeniy Roschin <evgeny689@yandex.ru>
2019-11-29 12:11:43 +0400

    HW ajs-3.2

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+import sortHeroes from '../app';
+
+test('different', () => {
+  const heroesArr = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  expect(sortHeroes(heroesArr)).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,14 @@
 // TODO: write your code here
-import sum from './basic';
+// import sum from './basic';
 
-console.log('worked');
+// console.log('worked');
 
-console.log(sum([1, 2]));
+// console.log(sum([1, 2]));
+
+function sortHeroes(heroes) {
+  heroes.sort((prev, next) => next.health - prev.health);
+
+  return heroes;
+}
+
+export default sortHeroes;
